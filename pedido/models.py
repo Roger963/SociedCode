70827aede67e037a063be9d594116be19cfc0ce1
Roger Pineda <roger.pinedapaz@gmail.com>
2022-08-02 22:56:49 -0500

    arreglando modelos

--- pedido/models.py
+++ pedido/models.py
@@ -4,10 +4,15 @@ from product.models import Product
 # Create your models here.
 class Order(models.Model):
     provider = models.ForeignKey(Provider, on_delete=models.CASCADE)
+    ordernumber = models.IntegerField()
     orderdate = models.DateTimeField(auto_now_add=True)
     senddate = models.DateTimeField(auto_now=True)
 
+
 class Detalle(models.Model):
+    
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    filedetalle = models.FileField()
     cantidad = models.IntegerField(default=1)
+    monto = models.IntegerField()
